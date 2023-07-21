a6bf91723ccab0608042c0fe4d41c239212531af
Maksim Lin <maks@manichord.com>
2023-07-21 12:10:54 +1000

    minimal static httpd for serving admin app

--- bin/fsd.dart
+++ bin/fsd.dart
@@ -1,9 +1,32 @@
 // import 'package:server/test_server.dart';
+import 'dart:io';
+
+import 'package:petit_httpd/petit_httpd.dart';
 import 'package:server/websocket_server.dart';
 
 void main(List<String> arguments) async {
   // final server = FSDServer();
+  
+  // start websocket server
   final server = WSServer();
+  server.start(9090);
 
-  await server.start(9090);
+  var petitHTTPD = PetitHTTPD(
+    Directory('admin_app'),
+    port: 8080,
+    // securePort: 443,
+    bindingAddress: '0.0.0.0',
+    // letsEncryptDirectory: Directory('/etc/letsencrypt/live'),
+    // domains: {
+    //   'fastdart.dev': 'contact@fastdart.dev',
+    // },
+  );
+
+  final ok = await petitHTTPD.start();
+  if (!ok) {
+    print('** ERROR Starting: $petitHTTPD');
+    exit(1);
+  }
+
+  print('-- HTTP server listening on localhost:${petitHTTPD.port} docs:${petitHTTPD.documentRoot.path}');
 }
--- lib/websocket_server.dart
+++ lib/websocket_server.dart
@@ -4,11 +4,12 @@ class WSServer {
   late final HttpServer _server;
 
   Future<void> start(int port) async {
+    const path = '/ws';
     _server = await HttpServer.bind(InternetAddress.loopbackIPv4, port);
-    print('Listening on localhost:${_server.port}');
+    print('-- WS server listening on localhost:${_server.port} path:$path');
 
     await for (HttpRequest request in _server) {
-      if (request.uri.path == '/ws') {
+      if (request.uri.path == path) {
         // Upgrade an HttpRequest to a WebSocket connection
         final socket = await WebSocketTransformer.upgrade(request);
         print('Client connected!');
